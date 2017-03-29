876984f342579ede235a4689c097984bae78d3b1
Owen Diehl <ow.diehl@gmail.com>
2017-03-29 18:00:56 -0400

    getting better parsing

--- example.js
+++ example.js
@@ -11,6 +11,12 @@ utils.fetchApartments()
     utils.fetchIndividualListing(matches[88])
       .then(utils.transformPage)
       .then(x => {
-        console.log(x)
+        let body = x[2].children[3];
+        let wanted = body.children
+            .filter(x => x.children && x.tagName !== 'script')[0]
+            .children
+            .filter(x => x.children);
+        console.log(wanted);
+        debugger;
       });
   });
