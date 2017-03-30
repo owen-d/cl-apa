c71a872f88c700c0af67744b8c5292967f742784
Owen Diehl <ow.diehl@gmail.com>
2017-03-30 17:39:58 -0400

    basic functionality working; still need to add support for querying last execution time

--- example.js
+++ example.js
@@ -7,19 +7,32 @@ const _ = require('lodash');
 const fs = require('fs');
 const moment = require('moment-timezone');
 
+const config = {
+  slackUrl: process.env.SLACK_URL,
+  waitInterval: 2000,
+  notAfterUnixDate: process.env.AFTER_DATE || moment().subtract(45, 'minutes').unix()
+};
+
 utils.fetchApartments({
-  notAfterUnixDate: moment().subtract(1, 'hour').unix()
+  notAfterUnixDate: config.notAfterUnixDate
 })
   .then(matches => {
+    console.log('matched %d', matches.length);
     return Bb.map(matches, match => {
       return utils.fetchIndividualListing(match)
         .then(utils.transformPage)
         .then(utils.parsePage)
         .then(parsed => ({
           url: match,
-          listing: parsed
-        }));
-    });
+          listing: parsed,
+          link: match
+        }))
+        .then(utils.formatSlackMessage);
+    })
+      .then(formatted => _.reduce(formatted, (accum, cur) => {
+        return accum
+          .then(utils.postToSlack(config.slackUrl, cur))
+          .delay(config.waitInterval);
+      }, Bb.resolve()));
   });
-
 
