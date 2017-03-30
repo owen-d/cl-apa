c681d375c9e131f5ab504a2dd6a82f7acc6b8e1f
Owen Diehl <ow.diehl@gmail.com>
2017-03-30 19:18:51 -0400

    runs off a unix time file to only update recently added

--- example.js
+++ example.js
@@ -8,17 +8,34 @@ const fs = require('fs');
 const moment = require('moment-timezone');
 
 const config = {
-  slackUrl: process.env.SLACK_URL,
+  slackToken: process.env.SLACK_TOKEN,
   waitInterval: 2000,
-  notAfterUnixDate: process.env.AFTER_DATE || moment().subtract(45, 'minutes').unix()
+  notAfterUnixDate: ['30', 'minutes'],
+  channel: 'mt-pleasant-apts',
+  unixChannel: 'mt-pleasant-apts-unix'
 };
 
-utils.fetchApartments({
-  notAfterUnixDate: config.notAfterUnixDate
-})
+
+
+utils.findChannelId({name: config.unixChannel, token: config.slackToken})
+  .then(id => {
+    if (!id) {
+      throw new Error('no matching channel');
+    }
+    return utils.getLastUnix({channel: id, token: config.slackToken});
+  })
+  .then(wasFound => {
+    if (wasFound) {
+      return wasFound;
+    } else {
+      return moment().subtract(...config.notAfterUnixDate).unix();
+    }
+  })
+  .then(notAfterUnixDate => {
+    return utils.fetchApartments({notAfterUnixDate});
+  })
   .then(matches => {
-    console.log('matched %d', matches.length);
-    return Bb.map(matches, match => {
+    return Bb.map(matches.slice(0,2), match => {
       return utils.fetchIndividualListing(match)
         .then(utils.transformPage)
         .then(utils.parsePage)
@@ -28,11 +45,26 @@ utils.fetchApartments({
           link: match
         }))
         .then(utils.formatSlackMessage);
-    })
-      .then(formatted => _.reduce(formatted, (accum, cur) => {
-        return accum
-          .then(utils.postToSlack(config.slackUrl, cur))
-          .delay(config.waitInterval);
-      }, Bb.resolve()));
+    });
+  })
+  .then(formatted => {
+    let now = moment().unix();
+    return _.reduce(formatted, (accum, cur) => {
+      return accum
+        .then(() => utils.postToSlack({
+          token: config.slackToken,
+          channel: config.channel,
+          attachments: cur.attachments,
+          // for some reason needs empty text param when attachments are specified.
+          text: ''
+        }))
+        .delay(config.waitInterval);
+    }, Bb.resolve())
+      .then(() => {
+        return utils.postToSlack({
+          token: config.slackToken,
+          channel: config.unixChannel,
+          text: now + ''
+        });
+      });
   });
-
